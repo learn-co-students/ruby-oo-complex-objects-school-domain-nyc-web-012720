508bfbb12b7d9712a98bd0fa95ad2f84aa886a7f
osgoodgunawan <ogunawan@ucsd.edu>
2020-01-29 18:56:29 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,45 @@
-# code here!
+class School
+
+    def initialize(school)
+        @school=school
+        @roster={}
+
+    end
+
+     def roster=(roster)
+       @roster=roster
+     end
+
+
+     def roster
+        @roster
+     end
+
+
+     def add_student(student,grade)
+        
+        if @roster[grade]!= nil
+            @roster[grade] << student   
+        else
+            @roster[grade]=[]
+            @roster[grade] << student
+        end  
+
+        @roster  
+     end
+
+     def grade (grade)
+        @roster[grade]
+     end
+
+     def sort
+         @roster.each do |k,v| v.sort! 
+         end
+     end
+
+
+end
+
+
+
+# school = School.new("Bayside High School")
