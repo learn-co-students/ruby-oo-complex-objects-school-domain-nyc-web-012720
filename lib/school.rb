0d7043c6599d5c4a684f350662112a0c04f98d93
osgoodgunawan <ogunawan@ucsd.edu>
2020-01-29 20:44:04 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,21 @@
 class School
 
+    attr_accessor :roster
+
     def initialize(school)
         @school=school
         @roster={}
 
     end
 
-     def roster=(roster)
-       @roster=roster
-     end
+    #  def roster=(roster)
+    #    @roster=roster
+    #  end
 
 
-     def roster
-        @roster
-     end
+    #  def roster
+    #     @roster
+    #  end
 
 
      def add_student(student,grade)
