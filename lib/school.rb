d8456090da478e48eedb78bae82fff486bd45a65
Jared Fleming <jaredmf10@gmail.com>
2020-01-30 08:44:58 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+clas School
+    attr_accessor :name, :student, :grade
+
+    def initialize(name)
+        name = @name
+        student = @student
+    end
+
+    def roster
+        roster = {}
+    end
+    
+    def add_student(student, grade)
+        roster[:grade] = []
+        roster[:grade] << student
+    end
+
+end
+
+school = School.new("Bayside High School")
+
